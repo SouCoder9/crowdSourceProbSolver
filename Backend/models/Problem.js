a7a27fdf1869e85c9baa63e9c5e593803e7905bd
soucoder9 <soubhadra1234@gmail.com>
2025-02-03 03:29:06 +0530

    removed garbage files

--- Backend/models/Problem.js
+++
@@ -1,8 +0,0 @@
-const problemSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    category: { type: String, enum: ['community', 'business', 'tech'] },
-    postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    createdAt: { type: Date, default: Date.now },
-    status: { type: String, enum: ['open', 'closed'], default: 'open' }
-  });
